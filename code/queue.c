16a42a96c14020636757fb45517d6060d3dc5f44
paul mcdowell <paul mcdowell>
2016-11-03 17:32:09 -0500

    bypassed asm volatile code for safer alternative

--- code/queue.c
+++ code/queue.c
@@ -61,16 +61,52 @@ void CQueue::ProcessQueue()
 					.J15:
 						");
 		   */
-		   	for(int r = 0; r < qf->queue_numargs; r++)
+
+		   	// 20161103 - rezalas
+		   	// for(int r = 0; r < qf->queue_numargs; r++)
+			// {
+
+            //             	asm volatile("" \
+            //                     "movl    %0, %%eax\n\t" \
+            //                     "pushl   %%eax\n\t"
+			// 	:
+            //                     : "r"(qf->queue_args[r])
+            //                     : "%eax");
+			// }
+			// (*qf->queue_function) ();
+
+
+			switch(qf->queue_numargs)
 			{
-                        	asm volatile("" \
-                                "movl    %0, %%eax\n\t" \
-                                "pushl   %%eax\n\t"
-				:
-                                : "r"(qf->queue_args[r])
-                                : "%eax");
+				case 0:
+					(*qf->queue_function)();
+					break;
+				case 1:
+					(*qf->queue_function)(qf->queue_args[0]);
+					break;
+				case 2:
+					(*qf->queue_function)(qf->queue_args[0],qf->queue_args[1]);
+					break;
+				case 3: 
+					(*qf->queue_function)(qf->queue_args[0],qf->queue_args[1],qf->queue_args[2]);
+					break;
+				case 4:
+					(*qf->queue_function)(qf->queue_args[0],qf->queue_args[1],qf->queue_args[2],qf->queue_args[3]);
+					break;
+				case 5:
+					(*qf->queue_function)(qf->queue_args[0],qf->queue_args[1],qf->queue_args[2],qf->queue_args[3],qf->queue_args[4]);
+					break;
+				case 6:
+					(*qf->queue_function)(qf->queue_args[0],qf->queue_args[1],qf->queue_args[2],qf->queue_args[3],qf->queue_args[4],qf->queue_args[5]);
+					break;
+				case 7:
+					(*qf->queue_function)(qf->queue_args[0],qf->queue_args[1],qf->queue_args[2],qf->queue_args[3],qf->queue_args[4],qf->queue_args[5],qf->queue_args[6]);
+					break;
+				case 8:
+					(*qf->queue_function)(qf->queue_args[0],qf->queue_args[1],qf->queue_args[2],qf->queue_args[3],qf->queue_args[4],qf->queue_args[5],qf->queue_args[6],qf->queue_args[7]);
+					break;
 			}
-			(*qf->queue_function) ();
+
 			for(int r = 0; r < qf->queue_numargs; r++)
 				asm volatile("popl %%eax" : : : "%eax");
 			qf->FreeQueue();
