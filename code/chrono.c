73adb318b203c2b74c836409302456e3c8760feb
Sean Gilliam <sean.gilliam@gmail.com>
2019-10-12 15:33:29 -0500

    created chrono header

--- code/chrono.c
+++ code/chrono.c
@@ -1,23 +1,4 @@
-#include <sys/types.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <math.h>
-#include "merc.h"
-#include "magic.h"
-#include "recycle.h"
-#include "db.h"
-#include "lookup.h"
-#include "tables.h"
-
-void extract_rune args((RUNE_DATA *rune));
-RUNE_DATA *find_rune args((void *vo, int target_type, int trigger_type, RUNE_DATA *rune_prev));
-void draw_rune(void *vo, void *vo2);  //attempt to add a rune that's just been initiated
-void apply_rune(RUNE_DATA *rune); //make new copy of rune and apply it to target
-
-bool activate_stasis_wall(void *vo,void *vo2,void *vo3,void *vo4);	//list your rune trigs here for good reference
-bool trigger_stasis_wall(void *vo,void *vo2,void *vo3,void *vo4);
+#include "chrono.h"
 
 void spell_stasis_wall( int sn, int level, CHAR_DATA *ch, void *vo,int target )
 {
