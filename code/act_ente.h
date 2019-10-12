20d85e518aa108ede4dc490c4319c029ec3c1db0
Sean Gilliam <sean.gilliam@gmail.com>
2019-10-12 15:29:46 -0500

    created act_ente header

---
+++ code/act_ente.h
@@ -0,0 +1,21 @@
+#ifndef ACT_ENTE_H
+#define ACT_ENTE_H
+
+#include <sys/types.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "merc.h"
+
+/* command procedures needed */
+DECLARE_DO_FUN(do_look);
+DECLARE_DO_FUN(do_stand);
+DECLARE_SPELL_FUN(spell_teleport);
+
+/* random room generation procedure */
+ROOM_INDEX_DATA *get_random_room(CHAR_DATA *ch);
+/* RT Enter portals */
+void do_enter( CHAR_DATA *ch, char *argument);
+
+#endif /* ACT_ENTE_H */
