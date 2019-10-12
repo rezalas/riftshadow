404d0702e7e153f7e2c19a3d2d5bbc63069565ec
Sean Gilliam <sean.gilliam@gmail.com>
2019-10-12 15:32:09 -0500

    createds ban header

---
+++ code/ban.h
@@ -0,0 +1,17 @@
+#ifndef BAN_H
+#define BAN_H
+
+#include <sys/types.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "merc.h"
+#include "recycle.h"
+#include "time.h"
+
+bool check_ban (char *usite, int type, int host);
+void do_ban (CHAR_DATA *ch, char *argument);
+void do_unban (CHAR_DATA *ch, char *argument);
+
+#endif /* BAN_H */
