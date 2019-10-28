7d6dc5bd5c983b1ee28ebc12870d68b4530caf97
Charlie Wilkins <charlie@101a.co.uk>
2019-10-28 14:32:12 +0000

    Revert "Revert "Remove four assignments in conditionals in ap.c""
    This reverts commit 1590267ee90e87a65d00fc58e949a0509c888f96.

--- code/ap.c
+++ code/ap.c
@@ -4,10 +4,10 @@
 void check_bloodlust(CHAR_DATA *ch, CHAR_DATA *victim)
 {
 	AFFECT_DATA *af_old, af;
-	int skill;
+	int skill = get_skill(ch, gsn_bloodlust);
 	int level = ch->level;
 	
-	if ((skill = get_skill(ch, gsn_bloodlust)) < number_percent()) {
+	if (skill < number_percent()) {
 		check_improve(ch,gsn_bloodlust,FALSE,1);
 		return;
 	}
@@ -48,9 +48,11 @@ void check_bloodlust(CHAR_DATA *ch, CHAR_DATA *victim)
 
 void check_leech(CHAR_DATA *ch, CHAR_DATA *victim)
 {
-	int leeched, skill;
+	int leeched;
+
+	int skill = get_skill(ch, gsn_leech);
 	
-	if ((skill = get_skill(ch,gsn_leech)) < number_percent())
+	if (skill < number_percent())
 		return;
 	
 	if (IS_SET(victim->imm_flags,IMM_MAGIC))
@@ -1972,9 +1974,9 @@ void burning_pulse(CHAR_DATA *ch, AFFECT_DATA *af)
 void do_darksight(CHAR_DATA *ch, char *argument)
 {
 	AFFECT_DATA af;
-	int number;
+	int number = get_skill(ch, gsn_darksight);
 
-	if ( (number = get_skill(ch,gsn_darksight)) == 0 ||
+	if ( number == 0 ||
 		ch->level < skill_table[gsn_darksight].skill_level[ch->Class()->GetIndex()] )
 		return send_to_char("Huh?\n\r", ch);
 
@@ -1987,7 +1989,9 @@ void do_darksight(CHAR_DATA *ch, char *argument)
 	if (ch->mana < 40)
 		return send_to_char("You don't have enough mana.\n\r",ch);
 
-	if ( (number = number_percent()) > get_skill(ch, gsn_darksight) ) {
+	number = number_percent(); //This was in the below conditional
+
+	if ( number > get_skill(ch, gsn_darksight) ) {
 		send_to_char("You peer into the shadows but your vision stays the same.\n\r",ch);
 		check_improve(ch,gsn_darksight,FALSE, 2);
 		ch->mana -= 20;
