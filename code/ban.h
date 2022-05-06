a746f8d091b4139ccd4d03d8e70b16cea2887496
Sean Gilliam <sean.gilliam@gmail.com>
2022-05-06 03:03:53 +0000

    added ban test debugger launch + ban table doc

--- code/ban.h
+++ code/ban.h
@@ -4,6 +4,19 @@
 #include "merc.h"
 #include "newmem.h"
 
+//	== table layout ==
+//	CREATE TABLE `bans` (
+//		`site` varchar(254) NOT NULL default '',
+//		`by` varchar(50) NOT NULL default '',
+//		`reason` varchar(100) NOT NULL default '',
+//		`date` datetime default NULL,
+//		`duration` bigint(20) NOT NULL default '0',
+//		`ban_type` int(11) NOT NULL default '0',
+//		`host_type` int(11) NOT NULL default '0'
+//	) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
 //
 // Site ban structure.
 //
