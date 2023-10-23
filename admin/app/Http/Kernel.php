7da4db7cb0437092d783819a0bc1be1571a69209
daedalus11069 <daedalus969@gmail.com>
2023-10-22 19:00:26 -0700

    Update Laravel framework 9.x -> 10.x

--- admin/app/Http/Kernel.php
+++ admin/app/Http/Kernel.php
@@ -58,7 +58,7 @@ class Kernel extends HttpKernel
 	 *
 	 * @var array
 	 */
-	protected $routeMiddleware = [
+	protected $middlewareAliases = [
 		'admin' => \App\Http\Middleware\Admin::class,
 		'auth' => \App\Http\Middleware\Authenticate::class,
 		'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
