037d22721fe70459c192b6e6d5c1bd2adbead576
Sean Gilliam <sean.gilliam@gmail.com>
2026-07-22 22:05:34 +0000

    Moving reset_data out of merc.h

--- code/merc.h
+++ code/merc.h
@@ -1740,32 +1740,7 @@ struct obj_apply_data
 
 
 
-//
-// Reset commands:
-//   '*': comment
-//   'M': read a mobile 
-//   'O': read an object
-//   'P': put object in object
-//   'G': give object to mobile
-//   'E': equip object to mobile
-//   'D': set state of door
-//   'R': randomize room exits
-//   'S': stop (end of list)
-//
-
-//
-// Area-reset definition.
-//
-
-struct reset_data
-{
-	RESET_DATA *next;
-	char command;
-	short arg1;
-	short arg2;
-	short arg3;
-	short arg4;
-};
+#include "entity/reset_data.h"
 
 
 //
