0c273dc19c2a87ba2b093e8178a6058dcaac422e
Sean Gilliam <sean.gilliam@gmail.com>
2026-07-22 21:54:44 +0000

    Moving exit_data out of merc.h

--- code/merc.h
+++ code/merc.h
@@ -1736,27 +1736,7 @@ struct obj_apply_data
 };
 
 
-//
-// Exit data.
-//
-
-struct exit_data
-{
-	union
-	{
-		ROOM_INDEX_DATA *to_room;
-		short vnum;
-	} u1;
-
-	long exit_info[MAX_BITVECTOR];
-	short key;
-	char *keyword;
-	char *description;
-	EXIT_DATA *next;					// OLC
-	int orig_door;						// OLC
-	RUNE_DATA *rune;
-	bool has_rune;
-};
+#include "entity/exit_data.h"
 
 
 
